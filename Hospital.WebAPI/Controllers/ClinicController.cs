73ae6c76d030f13b42c6c844b09d25deb927ede8
remzy abdulmoen <remzy.moen@hotmail.com>
2017-12-29 21:14:58 -0500

    updated Clinic

--- Hospital.WebAPI/Controllers/ClinicController.cs
+++ Hospital.WebAPI/Controllers/ClinicController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hospital.WebAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,44 @@ namespace Hospital.WebAPI.Controllers
 {
     public class ClinicController : ApiController
     {
+
+        public IHttpActionResult Get()
+        {
+            ClinicViewModel clinicViewModel = new ClinicViewModel();
+            clinicViewModel.Id = 4;
+            clinicViewModel.Name = "Bright Smiles";
+
+            return Ok(clinicViewModel);
+        }
+
+
+        /// <summary>
+        ///    content-type: application/json
+        ///    {        "Id": 1,"Name": "2"
+        ///    </summary>
+        /// <param name="clinicViewModel"></param>
+        /// <returns></returns>
+        public IHttpActionResult Post(ClinicViewModel clinicViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+            
+        }
+
+        public IHttpActionResult Put()
+        {
+            return Ok();
+        }
+        public IHttpActionResult Delete()
+        {
+            return Ok();
+        }
+
     }
 }
